e6c4b62606321710f498835eff57746f64a74574
White Night awa <evan.tang.2021@gmail.com>
2021-11-06 23:05:29 +0800

    update preload list;

--- js/preloader.js
+++ js/preloader.js
@@ -23,9 +23,10 @@ preload.on("progress", e => {
 
 
 const files = {
-    "background": ['rate1_bg.png', 'rate2_bg.png', 'rate3_bg.png', 'rate4_bg.png', 'rate5_bg.png'],
+    "background": ['rate0_item_bg.png', 'rate1_item_bg.png', 'rate2_item_bg.png', 'rate3_item_bg.png', 'rate4_item_bg.png'],
     "factions": ['Abyssal_Hunters.png', 'Aegir.png', 'Babel.png', 'Blacksteel.png', 'BolFvar.png', 'Columbia.png', 'Followers.png', 'Glasgow.png', 'Higashi.png', 'Iberia.png', 'Karlan_Commercial.png', 'Kazimierz.png', 'Laterano.png', 'Lees_Detective_Agency.png', 'Lungmen.png', 'Lungmen_Guard_Department.png', 'Minos.png', 'Penguin_Logistics.png', 'Rhine_Lab.png', 'Rhodes_Island.png', 'RI_Elite_Op.png', 'RI_Op_A4.png', 'RI_Op_Reserve_A1.png', 'RI_Op_Reserve_A4.png', 'RI_Op_Reserve_A6.png', 'Rim_Billiton.png', 'S.W.E.E.P..png', 'Sami.png', 'Sargon.png', 'Siesta.png', 'Siracusa.png', 'Team_Rainbow.png', 'Ursus.png', 'Ursus_Student_Self-Governing_Group.png', 'Victoria.png', 'Yen.png'],
     "Upgrade_materials": ['mat.svg'],
+    "items": ['Orirock.png'],
     "others": ['cursor.png', 'cursor.svg'],
 }
 
